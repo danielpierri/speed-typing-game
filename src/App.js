a24e185b6d0ecb7b289427ed6958306ac216a0c0
Daniel Pierri <cyberpierri@gmail.com>
2022-05-19 17:51:51 -0300

    Setting up to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import useWordGame from "./hooks/useWordGame"
 
 function App() {
+  const {
+    textBoxRef, 
+    handleChange, 
+    text, 
+    isTimeRunning, 
+    timeRemaining, 
+    startGame, 
+    wordCount
+  } = useWordGame(5)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Speed Typing Game</h1>
+      <h4>How fast do you type?</h4>
+      <textarea
+        ref={textBoxRef}
+        value={text}
+        onChange={handleChange}
+        disabled={!isTimeRunning}
+      />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button
+        onClick={startGame}
+        disabled={isTimeRunning}
+      >
+        Start
+      </button>
+      <h1>Word count: {wordCount}</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
